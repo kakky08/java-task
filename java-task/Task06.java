e5af115135c82149bb692ed3cbb857b0d943e5ba
kakky08 <77216691+kakky08@users.noreply.github.com>
2021-03-28 21:53:24 +0900

    累乗の修正

--- java-task/Task06.java
+++ java-task/Task06.java
@@ -2,8 +2,9 @@
 
 public class Task06 {
   public static void main(String[] args) {
+    int mul = 1;
     for (int i = 1; i <= 8; i++) {
-      int mul = 2 * (2 * i);
+      mul = mul * 2;
       System.out.println("2 の " + i + "乗 = " + mul);
     }
   }
