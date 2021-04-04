4168d62fbfd66cb4a56e51ae5d7a29e5c7650811
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-04-04 14:45:59 +0900

    テキストデータの配列を作成

--- java-typing/Typing.java
+++ java-typing/Typing.java
@@ -1,3 +1,14 @@
-public class Typing {
+import java.util.ArrayList;
 
+public class Typing {
+  public static void main(String[] args) {
+    ArrayList<String> dataset = new ArrayList<String>();
+    dataset.add("test");
+    dataset.add("test2");
+    dataset.add("test3");
+
+    for (String s : dataset) {
+      System.out.println(s);
+    }
+  }
 }
